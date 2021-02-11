92f65250b19cb5280ebe20af81baccb503077eae
lemaire <lucas.lemaire.sin@gmail.com>
2021-02-11 09:03:42 +0100

    Ajoutez des fichiers projet.

---
+++ ClassLibrary1/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1
+{
+    [TestClass]
+    class UppercaseTest
+    {
+        [TestMethod]
+        public void TestTrueEqualTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ ClassLibrary1/Uppercase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Uppercase
+    {
+        public string toUppercase(string toUppercase)
+        {
+            return toUppercase.ToUpper();
+        }
+    }
+}
